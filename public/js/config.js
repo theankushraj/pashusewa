08ac63bb8ffe1675d1ce72119d95d04a159d89a1
Ankush Raj <145738358+theankushraj@users.noreply.github.com>
2025-04-30 00:54:09 +0530

    Delete public directory

--- public/js/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * PashuSewa - Configuration
- * Contains API URL configuration
- */
-
-// Set the API_URL to your Cloudflare Worker URL
-const API_URL = 'https://pashusewa.pashusewa-india.workers.dev';
